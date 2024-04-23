c93c77bfd488c2d6d4162ce394b9537bc2520942
Micheline okito <okitobibi2@gmail.com>
2024-04-23 12:07:42 +0100

    Initialisation projet

---
+++ src/components/navbar.jsx
@@ -0,0 +1,21 @@
+
+
+const Navbar = () =>{
+
+    return(
+        <div className="sticky top-0 bg-white z-10">
+            <div className="continuer hidden lg:block">
+                <div className="flex justify-between items-center p-8">
+                    <h1 className="text-xl font-medium ">BIBI WENZE</h1>
+
+                </div>
+
+            </div>
+
+
+    
+        </div>
+    )
+}
+
+export default Navbar;
